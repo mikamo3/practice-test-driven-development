09aea6c01bf83041c27924d1b60b714f770c4562
mikamo3 <kamo3proj@gmail.com>
2021-03-09 23:34:33 +0900

    :construction: to No.443

---
+++ src/test/java/com/mycompany/app/MoneyTest.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+}

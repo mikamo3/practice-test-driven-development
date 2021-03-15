207598c4befae381f07ac9c6053479ae096eb91a
mikamo3 <kamo3proj@gmail.com>
2021-03-15 14:54:39 +0000

    :construction: to No.1011

--- src/main/java/com/mycompany/app/Money.java
+++ src/main/java/com/mycompany/app/Money.java
@@ -1,10 +1,20 @@
 package com.mycompany.app;
 
-public class Money {
+abstract class Money {
     protected int amount;
+
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount && this.getClass().equals(money.getClass());
+    }
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Franc franc(int amount) {
+        return new Franc(amount);
     }
 }
--- src/test/java/com/mycompany/app/MoneyTest.java
+++ src/test/java/com/mycompany/app/MoneyTest.java
@@ -9,23 +9,24 @@ import org.junit.Test;
 public class MoneyTest {
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     public void testEquality() {
-        assertTrue(new Dollar(10).equals(new Dollar(10)));
-        assertFalse(new Dollar(11).equals(new Dollar(10)));
-        assertTrue(new Franc(10).equals(new Franc(10)));
-        assertFalse(new Franc(11).equals(new Franc(10)));
+        assertTrue(Money.dollar(10).equals(Money.dollar(10)));
+        assertFalse(Money.dollar(11).equals(Money.dollar(10)));
+        assertTrue(Money.franc(10).equals(Money.franc(10)));
+        assertFalse(Money.franc(11).equals(Money.franc(10)));
+        assertFalse(Money.franc(10).equals(Money.dollar(10)));
     }
 
     @Test
     public void testFrancMultiplication() {
-        Franc five = new Franc(5);
-        assertEquals(new Franc(10), five.times(2));
-        assertEquals(new Franc(15), five.times(3));
+        Franc five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
     }
 }

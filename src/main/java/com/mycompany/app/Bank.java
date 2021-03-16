426632dbbfdde26249964626ed264265f3a2f7ef
mikamo3 <kamo3proj@gmail.com>
2021-03-16 14:34:46 +0000

    :construction: to No.1577

---
+++ src/main/java/com/mycompany/app/Bank.java
@@ -0,0 +1,7 @@
+package com.mycompany.app;
+
+public class Bank {
+    public Money reduce(Expression source, String to) {
+        return Money.dollar(10);
+    }
+}
--- src/main/java/com/mycompany/app/Money.java
+++ src/main/java/com/mycompany/app/Money.java
@@ -1,6 +1,6 @@
 package com.mycompany.app;
 
-class Money {
+class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -33,5 +33,9 @@ class Money {
 
     Money times(int multiplier) {
         return new Money(amount * multiplier, currency);
+    }
+
+    Money plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 }
